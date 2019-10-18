22c7b4cf10ba33515919772120d52d58d3a8e752
xxtractz <musambaloyi@gmail.com>
2019-10-18 07:08:47 +0200

    Added icon

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Profile page. */
 router.get('/', function(req, res, next) {
-  res.render('profile', {page:'Profile', menuId:'profile'});
+    res.render('profile', { page: 'Profile', menuId: 'profile' });
 });
 
 module.exports = router;
