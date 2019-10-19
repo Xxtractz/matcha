2abb0832f40b4277aac9f6b48b9ee719ed201176
xxtractz <musambaloyi@gmail.com>
2019-10-19 12:00:43 +0200

    DB connection and fixing ejs import errs

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ npm install
 npm install nodemon -g
 npm install express --save
 npm install ejs --save
-npm install mongodb passport passport-local express-session connect-flash
+npm install mongoose passport passport-local express-session connect-flash
