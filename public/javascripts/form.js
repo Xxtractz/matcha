80c749501ec8ae4882da4103f51ea6c1a9916555
xxtractz <musambaloyi@gmail.com>
2019-10-23 12:08:08 +0200

    working on Ajax validation

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,23 +1,29 @@
-// $(document).ready(function() {
-//             $(".login-form").on("submit", (function(e) {
-//                     e.preventDefault();
-//                     var data = {
-//                         email: $("#email").val(),
-//                         password: $("#password").val()
-//                     };
+$("login.ejs").ready(function() {
+    $(".login-form").submit(function(e) {
+        e.preventDefault();
+        var data = {
+            email: $("#email").val(),
+            password: $("#password").val()
+        };
 
-//                     $.ajax({
-//                         type: "POST",
-//                         url: "/api/login",
-//                         data: data,
-//                         success: function(sas) {
-//                             console.log("success");
-//                             data = {};
-//                         },
-//                     });
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/user/api/login",
+            data: data,
+            success: function(success) {
+                console.log("success");
+                console.log(data);
+                console.log(sas);
 
-//                 });
-//             });
-// $(document).ready(function () {
 
-// });
+                data = {};
+                console.log(data);
+            },
+            error: function(error) {
+                console.log(error.responseText);
+            }
+
+        });
+
+    });
+});
