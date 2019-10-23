908b158d00d38b8423514e373fe6b8d29ea5e80b
xxtractz <musambaloyi@gmail.com>
2019-10-23 21:57:23 +0200

    working on Ajax form validation

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -16,7 +16,7 @@ $("login.ejs").ready(function() {
                 window.location.replace("http://localhost:3000/dashboard");
             },
             error: function(error) {
-                $(".login-form").addClass("was-validated");
+                // $(".login-form").addClass("was-validated");
                 if (error.responseText == "Incorrect email.") {
                     var txt = $('<div class="invalid-feedback"></div>').text("Incorrect email."); // Create text with jQuery
                     $("#email-form").after(txt); // Append new elements
