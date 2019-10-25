e84c602d68c5f856419f934a742f70b6727e6426
xxtractz <musambaloyi@gmail.com>
2019-10-25 22:45:53 +0200

    auth guard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const auth = require("../services/authguard")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
     res.render('dashboard', { page: 'Dashboard', menuId: 'dashboard' });
 });
 
