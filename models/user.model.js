41e23303a417ba56bb5cd84b0e892dd2ca357255
xxtractz <musambaloyi@gmail.com>
2019-10-26 11:02:56 +0200

    removing unallowed libraries

--- models/user.model.js
+++ models/user.model.js
@@ -29,15 +29,4 @@ const User = mongoose.model('User', new mongoose.Schema({
     }
 }));
 
-function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(2).max(50).required(),
-        lastname: Joi.string().min(2).max(50).required(),
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    };
-    return Joi.validate(user, schema);
-}
-
 exports.User = User;
-exports.validate = validateUser;
