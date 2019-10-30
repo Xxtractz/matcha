10c3e37b10d92434620c92adbc75ae49ff55e272
xxtractz <musambaloyi@gmail.com>
2019-10-30 21:00:01 +0200

    updated server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require("mongoose");
+
+// Routers declare
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// DB Connection 
+const db_link = require('./config/keys').MongoUrl;
+//"mongodb+srv://xxtractz:Password1@matcha-ifa9u.mongodb.net/matcha?retryWrites=true&w=majority"
+mongoose.connect(`${db_link}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+
+// Run server on Port 4000
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server started on Port ${port}`));
+
+module.exports = app;
