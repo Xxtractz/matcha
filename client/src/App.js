0021d787e6b50e755387be253d3fc1123205ff28
unknown <musambaloyi@gmail.com>
2019-10-31 22:46:20 +0200

    working on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Login from './user/login';
+import Register from './user/register';
 
 class App extends Component {
     render(){
         return (
             <Router>
                 <Route exact path='/login' component={Login}/>
+                <Route exact path='/register' component={Register}/>
             </Router>
         )
     }
