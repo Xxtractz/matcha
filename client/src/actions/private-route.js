398780b355cf5bd4385b7a0352de88cfdae55dad
unknown <musambaloyi@gmail.com>
2019-11-02 22:21:46 +0200

    My Dad says i should keep coding

---
+++ client/src/actions/private-route.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Router} from 'react-router-dom;'
+import PropTypes from 'prop-types';
+
+
+const PrivateRoute = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+private-route.propTypes = {
+    
+};
+
+
+export default PrivateRoute;
+
