1af169832c1808e3e34b1246b5e9cbbe30c4210e
unknown <musambaloyi@gmail.com>
2019-11-03 21:36:08 +0200

    adding a side bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import React, {Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
+import SideNav from  './components/sidenav'
 import Login from './user/login';
 import Register from './user/register';
 
 class App extends Component {
     render(){
         return (
+            <div>
+                <SideNav/>
             <Router>
                 <Route exact path='/login' component={Login}/>
                 <Route exact path='/register' component={Register}/>
             </Router>
+            </div>
         )
     }
 }
