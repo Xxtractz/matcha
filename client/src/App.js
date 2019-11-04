9cfc73e5f493aef4efb4970f312e01b52b0e4c51
unknown <musambaloyi@gmail.com>
2019-11-04 12:53:05 +0200

    modifying files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, {Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { 
+    BrowserRouter as Router, 
+    Route,
+    Switch,
+    Redirect
+ } from 'react-router-dom';
 
 import './App.css';
 
@@ -7,15 +12,20 @@ import './App.css';
 import Home from './pages'
 import Login from './pages/login';
 import Register from './pages/register';
+import NotFound from './pages/error';
 
 class App extends Component {
     render(){
         return (
             <div>
             <Router>
-                <Route exact path="/" component={Home}/>
-                <Route exact path='/login' component={Login}/>
-                <Route exact path='/register' component={Register}/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/404" component={NotFound}/>
+                    <Redirect to="/404" />
+                </Switch>
             </Router>
             </div>
         )
