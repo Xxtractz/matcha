fe47ec459e193c11fbbe3997e7950625e5a74569
xxtractz <musambaloyi@gmail.com>
2019-12-26 21:00:13 +0200

    after Hours of debuging and trauma... finaly fixed the code

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from 'react-router-dom';
-import {Navbar, Nav, NavItem, Button, Glyphicon} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, Button} from 'react-bootstrap';
 // import {MDBBtn} from "mdbreact";
 import {logout} from "../middleware/auth";
 import {getUserName, getUserLastName} from "../middleware/user";
-import Sidebar from 'react-bootstrap-sidebar';
 
 class NavbarPage extends Component {
   constructor(){
@@ -29,15 +28,7 @@ render() {
   return (
     <Router>
                    <div>
-                  <Button bsStyle="primary" onClick={ () => this.updateModal(true) }><Glyphicon glyph="menu-hamburger"/></Button>
-                  <Sidebar side='left' isVisible={ this.state.isVisible } onHide={ () => this.updateModal(false) }>
-                    <Nav>
-                      <NavItem href="#">Link 1</NavItem>
-                      <NavItem href="#">Link 2</NavItem>
-                      <NavItem href="#">Link 3</NavItem>
-                      <NavItem href="#">Link 4</NavItem>
-                    </Nav>
-                  </Sidebar>
+                     nav
               </div>
      </Router>
     );
