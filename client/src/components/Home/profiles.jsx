ffb1a58310646e0b255385b597a4dc4ca2dc9762
xxtractz <musambaloyi@gmail.com>
2019-12-29 22:32:55 +0200

    added dummy content

--- client/src/components/Home/profiles.jsx
+++ client/src/components/Home/profiles.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
 
+import {Paper, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+
 class Profiles extends Component {
   render() {
     return (
-      <div> This Is Profiles </div>
+      <div className="container-fluid mb-4">
+        <Paper variant="outlined" square>
+          <div className=" row">
+            <div className="col m-4">
+            <Card>
+              Cars
+            </Card>
+            </div>
+            <div className="col m-4">
+            <Card>
+              Cars
+            </Card>
+            </div>
+            <div className="col m-4">
+              <Card className="">
+                <CardActionArea>
+                  <CardMedia
+                    className=""
+                    image=""
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                      across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          </div>
+          
+        </Paper>
+      </div>
     );
   }
 }
--- client/src/components/Home/search.jsx
+++ client/src/components/Home/search.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import {Paper} from '@material-ui/core';
+
 class Search extends Component {
   render() {
     return (
-      <div> Search and Results Table     </div>
+      <div className="container-fluid mb-4">
+        <Paper variant="outlined" square>
+          Im in Paper
+        </Paper>
+      </div>
     );
   }
 }
