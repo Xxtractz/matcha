1de3a85571dff215dcd7e13100758198e4778593
xxtractz <musambaloyi@gmail.com>
2019-12-31 19:40:31 +0200

    Moving things around

--- client/src/components/Home/tips.jsx
+++ client/src/components/Home/tips.jsx
@@ -5,9 +5,9 @@ import {Paper} from '@material-ui/core';
 class Tips extends Component {
   render() {
     return (
-      <div className="container mt-5 mb-4" style={{}}>
+      <div className="container mt-5 mb-4" style={{paddingTop:"120px"}}>
         <Paper className="text-center" variant="outlined" square>
-          How to 
+          
         </Paper>
       </div>
     );
