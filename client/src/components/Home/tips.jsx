7ffc28ba5eb47cdf22db34000b94a8edf9d1a27d
xxtractz <musambaloyi@gmail.com>
2019-12-31 19:36:18 +0200

    at last

---
+++ client/src/components/Home/tips.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import {Paper} from '@material-ui/core';
+
+class Tips extends Component {
+  render() {
+    return (
+      <div className="container mt-5 mb-4" style={{}}>
+        <Paper className="text-center" variant="outlined" square>
+          How to 
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default Tips;
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 // Components
 import Layout from '../components/Layout/layout'
 import Profiles from '../components/Home/profiles';
-import Search from '../components/Home/search';
+import Tips from '../components/Home/tips';
 
 class index extends Component {
     render() {
         return (
             <Layout>
-                <Search></Search>
                 <Profiles></Profiles>
+                <Tips></Tips>
             </Layout>
         )
     }
