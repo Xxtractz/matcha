34fa57bcc50ccd799ec49de5f28e7bdafc084723
xxtractz <musambaloyi@gmail.com>
2020-01-07 16:38:22 +0200

    register

--- client/src/middleware/auth.js
+++ client/src/middleware/auth.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const signInUrl = require('../utils/link').signInUrl;
+
 export const login = (data) => {
   localStorage.setItem("SessionUI", 'TestLogin');
   localStorage.setItem("user", '{"firstname":"Musa","lastname":"Baloyi"}')
@@ -7,7 +10,14 @@ export const logout = () => {
   localStorage.clear();
 }
 
-export const register = (data) =>{
+export const register = (_userdata) =>{
+  axios.post(signInUrl,_userdata )
+    .then(response => { 
+      console.log(response)
+    })
+    .catch(error => {
+        console.log(error.response)
+    });
   
 }
 
