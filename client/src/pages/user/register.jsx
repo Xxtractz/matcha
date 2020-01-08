edb794950b7bbf144edd78431dc0b49ac8f6461c
xxtractz <musambaloyi@gmail.com>
2020-01-08 10:12:16 +0200

    adding Comments

--- client/src/pages/user/register.jsx
+++ client/src/pages/user/register.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {register} from '../../middleware/auth';
 import {Button, TextField, Card, CardActions, ButtonBase} from '@material-ui/core';
+import {isYearValid} from '../../utils/validate';
 
 class Register extends Component {
 
@@ -10,30 +11,51 @@ class Register extends Component {
       fname:"",
       lname:"",
       username:"",
-      age:"",
+      username_err:"",
+      username_err_helperText:"",
+      year:"",
+      year_err:"",
+      year_err_helperText:"",
+      month:"",
+      month_err:"",
+      month_err_helperText:"",
+      day:"",
+      day_err:"",
+      day_err_helperText:"",
       email: "",
-      password: "",
       email_err:"",
       email_err_helperText:"",
+      password: "",
       password_err:"",
-      password_err_helperText:"",
+      password_err_helperText:""
     }
   }
 
   
   submitHandler = e =>{
     e.preventDefault();
-    const user = {
-      "fname": this.state.fname.toString(),
-      "lname": this.state.lname.toString(),
-      "username": "Musa",
-      "age": '18',
-      "email": this.state.email.toString(),
-      "password": this.state.password.toString(),
-    };
-    this.register(user);
+
+    if(this.isvalidated()){
+      const user = {
+        "fname": this.state.fname.toString(),
+        "lname": this.state.lname.toString(),
+        "username": "Musa",
+        "age": '18',
+        "email": this.state.email.toString(),
+        "password": this.state.password.toString(),
+      };
+      this.register(user);
+    }
   }
 
+  isvalidated(){
+    if(isYearValid(this.state.year)){
+      return true;
+    }
+    else{ 
+      return false;
+    }
+  }
   register(userData){
       if(register(userData)){
         window.alert("Registration Succesful");
@@ -52,16 +74,25 @@ class Register extends Component {
       <div>
         <div  className="container">
           <div className="row">
-            <div className="col-7 mx-auto pt-5 mt-5" >
+            <div className="col-md-8 mx-auto pt-5 mt-5" >
               <Card className="card m-5 p-5 mx-auto col-10">
-                {/* <form onSubmit={this.submitHandler}> */}
+
+                {/* Form Starts */}
                 <form onSubmit={this.submitHandler}>
+
+                  {/* Header Text Start */}
                   <p className="h3 text-center mb-4">Register</p>
                   <div className="text-center">
                     <small> Please Enter your Registration details below</small>
                   </div>
+                  {/* Header Text End */}
+
                   <hr className="mb-2 ml-5 mr-5"></hr>
+
+                  {/* Input Box Start */}
                   <div className="grey-text">
+
+                    {/* Fname and  lastName Row */}
                     <div className="row mb-3">
                       <div className="col-6 text-center">
                         <TextField 
@@ -69,8 +100,8 @@ class Register extends Component {
                           type="text" 
                           name="fname"
                           label="First Name"
-                          // helperText={this.state.email_err_helperText}
-                          // error={this.state.email_err ? true : false}
+                          helperText={this.state.fname_err_helperText}
+                          error={this.state.fname_err ? true : false}
                           value= {this.state.fname}
                           onChange={e => this.onChange(e)}
                           required
@@ -90,6 +121,8 @@ class Register extends Component {
                         />
                       </div>
                     </div> 
+
+                    {/* Age / Date of Birth  */}
                     <div className="row mb-3">
                       <div className="col text-center">
                         <TextField
@@ -98,6 +131,7 @@ class Register extends Component {
                           name="year"
                           type="text"
                           className="col-8"
+                          value= {this.state.year}
                           onChange={e => this.onChange(e)}
                           required
                         />
@@ -109,6 +143,7 @@ class Register extends Component {
                           name="month"
                           type="text"
                           className="col-8"
+                          value= {this.state.month}
                           onChange={e => this.onChange(e)}
                           required
                         />
@@ -120,11 +155,31 @@ class Register extends Component {
                           name="day"
                           type="text"
                           className="col-8"
+                          value= {this.state.day}
+                          onChange={e => this.onChange(e)}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    {/* Username */}
+                    <div className="row mb-3">
+                      <div className="col-12 text-center">
+                        <TextField 
+                          className="col-12"
+                          type="text" 
+                          name="username"
+                          label="Username"
+                          // helperText={this.state.email_err_helperText}
+                          // error={this.state.email_err ? true : false}
+                          value= {this.state.username}
                           onChange={e => this.onChange(e)}
                           required
                         />
                       </div>
                     </div> 
+
+                    {/* Email */}
                     <div className="row mb-3">
                       <div className="col-12 text-center">
                         <TextField 
@@ -140,6 +195,8 @@ class Register extends Component {
                         />
                       </div>
                     </div> 
+
+                    {/* Password */}
                     <div className="row mb-3">
                       <div className="col-12 text-center">
                         <TextField 
@@ -155,6 +212,8 @@ class Register extends Component {
                         />
                       </div>
                     </div>
+
+                    {/* Confirm Password  */}
                     <div className="row mb-3">
                       <div className="col-12 text-center">
                         <TextField 
@@ -170,7 +229,10 @@ class Register extends Component {
                         />
                       </div>
                     </div>
+                    
                   </div>
+                  {/* Input Box End */}
+
                   <div className="text-center p-3 mt-4">
                     <Button variant="contained" type="submit" >
                       Register
