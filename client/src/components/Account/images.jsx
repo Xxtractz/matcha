9f4b106b904dbecab1f5d4e5eeca3e6d2f659ae1
xxtractz <musambaloyi@gmail.com>
2020-01-15 19:37:06 +0200

    images

--- client/src/components/Account/images.jsx
+++ client/src/components/Account/images.jsx
@@ -1,98 +1,80 @@
 import React, { Component } from 'react';
-import { Button, Card, CardMedia, CardActions, CardActionArea } from "@material-ui/core";
+import {Paper, Card, CardMedia, CardActions, CardActionArea } from "@material-ui/core";
 
 class images extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-        <Card className="col-md p-2 m-3">
-          <CardActionArea>
-            <CardMedia
-              className="profile_img_Card"
-              image="src/assets/images/profile.png"
-              title="Contemplative Reptile"
-            />
+        <Paper className="container p-2  col-12" variant="outlined">
+          <Paper className="col-12 mt-2 p-1 text-center bg_secondary" variant="elevation">
+            <h1>Images</h1>
+            <small> All your images on your profile, These can be seen by whoever views your profile</small>
+          </Paper>
+          <div className="row">
+          <Card className="col-md-2 p-2 m-3">
+            <CardActionArea>
+              <CardMedia
+                className="profile_img_Card"
+                image="src/assets/images/profile.png"
+                title="Contemplative Reptile"
+              />
               <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </CardActionArea>
-        </Card>
-        <Card className="col-md p-2 m-3">
-          <CardActionArea>
-            <CardMedia
-              className="profile_img_Card"
-              image="src/assets/images/profile.png"
-              title="Contemplative Reptile"
-            />
+              </CardActions>
+            </CardActionArea>
+          </Card>
+
+          <Card className="col-md-2 p-2 m-3">
+            <CardActionArea>
+              <CardMedia
+                className="profile_img_Card"
+                image="src/assets/images/profile.png"
+                title="Contemplative Reptile"
+              />
               <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </CardActionArea>
-        </Card>
-        <Card className="col-dd p-2 m-3">
-          <CardActionArea>
-            <CardMedia
-              className="profile_img_Card"
-              image="src/assets/images/profile.png"
-              title="Contemplative Reptile"
-            />
+              </CardActions>
+            </CardActionArea>
+          </Card>
+
+
+          <Card className="col-md-2 p-2 m-3">
+            <CardActionArea>
+              <CardMedia
+                className="profile_img_Card"
+                image="src/assets/images/profile.png"
+                title="Contemplative Reptile"
+              />
               <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </CardActionArea>
-        </Card>
-        <Card className="col-md p-2 m-3">
-          <CardActionArea>
-            <CardMedia
-              className="profile_img_Card"
-              image="src/assets/images/profile.png"
-              title="Contemplative Reptile"
-            />
+              </CardActions>
+            </CardActionArea>
+          </Card>
+
+
+
+          <Card className="col-md-2 p-2 m-3">
+            <CardActionArea>
+              <CardMedia
+                className="profile_img_Card"
+                image="src/assets/images/profile.png"
+                title="Contemplative Reptile"
+              />
               <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </CardActionArea>
-        </Card>
-        <Card className="col-md p-2 m-3">
-          <CardActionArea>
-            <CardMedia
-              className="profile_img_Card"
-              image="src/assets/images/profile.png"
-              title="Contemplative Reptile"
-            />
+              </CardActions>
+            </CardActionArea>
+          </Card>
+
+
+          <Card className="col-md-2 p-2 m-3">
+            <CardActionArea>
+              <CardMedia
+                className="profile_img_Card"
+                image="src/assets/images/profile.png"
+                title="Contemplative Reptile"
+              />
               <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </CardActionArea>
-        </Card>
-        </div>
-      </div>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+          </div>
+        </Paper>
     );
   }
 }
