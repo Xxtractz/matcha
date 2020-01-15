af80c709dd10703d113586f638b3f9f35ed235ac
xxtractz <musambaloyi@gmail.com>
2020-01-15 19:42:07 +0200

    working on profile frontend

--- client/src/components/Account/profile.jsx
+++ client/src/components/Account/profile.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import {Paper, Card, CardMedia, CardActions, CardActionArea } from "@material-ui/core";
 
 class UserProfile extends Component {
   render() {
     return (
-      <div> UserProfile </div>
+      <Paper className="container p-2 mt-4 col-12">
+        <Paper className="col-12 mt-2 p-1 text-center bg_secondary" variant="elevation">
+            <h1>Profile</h1>
+            <small> Details about you </small>
+          </Paper>
+      </Paper>
     );
   }
 }
