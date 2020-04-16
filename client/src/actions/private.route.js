63ea6b250c5401e3fb2e710bdfc354cb3530f0f7
Musa Baloyi <musambaloyi@gmail.com>
2020-04-16 13:24:28 +0200

    login bug

--- client/src/actions/private.route.js
+++ client/src/actions/private.route.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { isloggedIn } from "./api";
-import { getUserStatus } from "./user";
+import { getUserStatus, isDataAvailavle } from "./user";
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (isloggedIn) {
+        if (isloggedIn()) {
           return <Component {...props} />;
         } else {
           return (
