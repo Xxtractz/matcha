ccbd2dde25f8aff0a0ad00538dfe64f7620d21d8
Musa Baloyi <musambaloyi@gmail.com>
2020-05-02 09:52:21 +0200

    #comment working on adding Complete Profile Section IMAGE

--- client/src/components/Account/AccountComponents/completeProfile.jsx
+++ client/src/components/Account/AccountComponents/completeProfile.jsx
@@ -4,17 +4,28 @@ import { Paper } from "@material-ui/core";
 class CompleteProfile extends Component {
   render() {
     return (
-      <Paper className="container p-2  col-12" variant="outlined">
+      <Paper
+        className="container p-2  bg-transparent col-12"
+        variant="outlined"
+        square
+      >
         <Paper
-          className="col-12 mt-2 p-1 text-center"
+          className="col-12 mt-2 p-1 text-center bg-transparent"
           variant="outlined"
         >
-            <h1>Complete Profile</h1>
-          <small>
-            {" "}
-            Please Complete you Profile
-          </small>
+          <h1>Complete Profile</h1>
+          <small> Please Complete you Profile</small>
         </Paper>
+
+        <div className="container p-2">
+          <div className="col-12 ">
+            <div className="row">
+              <img className="img-thumbnail" src="src/assets/images/addImage.png" alt="imagr"  style={{width: "150px", height: "150px"}} />
+              <input type="button" class="btn btn-secondary" id="btnChangePicture" value="Change" />
+              <input type="file" name="file"/>
+            </div>
+          </div>
+        </div>
       </Paper>
     );
   }
