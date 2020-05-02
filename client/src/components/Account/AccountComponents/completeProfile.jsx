d1f89559a885800fa6aa61d5f35f923a6ee31146
Musa Baloyi <musambaloyi@gmail.com>
2020-05-02 19:02:46 +0200

    Update Complete Profile

--- client/src/components/Account/AccountComponents/completeProfile.jsx
+++ client/src/components/Account/AccountComponents/completeProfile.jsx
@@ -26,6 +26,23 @@ class CompleteProfile extends Component {
     };
   }
 
+  submitHandler = (e) => {
+    e.preventDefault();
+
+    // const user = {
+    //   username: this.state.username.toString(),
+    //   password: this.state.password.toString(),
+    // };
+  };
+
+  onChange = (e) => {
+    console.log(e.target.name + ":" + e.target.value);
+
+    this.setState({
+      [e.target.name]: [e.target.value],
+    });
+  };
+
   imageSection() {
     return (
       <div className="container p-2">
@@ -49,6 +66,7 @@ class CompleteProfile extends Component {
       </div>
     );
   }
+
   // Form Sections
   nameSection() {
     return (
@@ -60,8 +78,7 @@ class CompleteProfile extends Component {
             type="text"
             name="firstname"
             defaultValue={this.state.firstname}
-            onChange={(e) => this.onChange(e)}
-            required
+            disabled
           />
         </div>
         <div className="col-6 ">
@@ -71,9 +88,147 @@ class CompleteProfile extends Component {
             type="text"
             name="lastname"
             defaultValue={this.state.lastname}
-            onChange={(e) => this.onChange(e)}
-            required
+            disabled
           />
+        </div>
+      </div>
+    );
+  }
+
+  genderSection() {
+    return (
+      <div className="row mb-3">
+        <div className="col-6 ">
+          <InputLabel>Gender</InputLabel>
+          <Select
+            native
+            // alue={getUserGender()}
+            // onChange={handleChange}
+            // inputProps={{
+            //   name: "age",
+            //   id: "age-native-simple",
+            // }}
+          >
+            <option value={"Male"}>Male</option>
+            <option value={"Other"}>Other</option>
+            <option value={"Female"}>Female</option>
+          </Select>
+        </div>
+        <div className="col-6 ">
+          <InputLabel>Preferred Gender</InputLabel>
+          <Select
+            native
+            // value={getUserGenderPreference()}
+            // onChange={handleChange}
+            // inputProps={{
+            //   name: "age",
+            //   id: "age-native-simple",
+            // }}
+          >
+            <option value={"Male"}>Male</option>
+            <option value={"Both"}>Both</option>
+            <option value={"Female"}>Female</option>
+          </Select>
+        </div>
+      </div>
+    );
+  }
+
+  bioSection() {
+    return (
+      <div className="row mb-3">
+        <div className="col-12">
+          <InputLabel>Bio</InputLabel>
+          {/* <TextField
+            className="col-12"
+            type="text"
+            name="fname"
+            // placeholder={getUserFirstName()}
+            // helperText={this.state.fname_err_helperText}
+            // error={this.state.fname_err ? true : false}
+            // value={this.state.fname}
+            // 
+            required
+          /> */}
+          <textarea
+            className="w-100"
+            aria-label="empty textarea"
+            name="bio"
+            defaultValue={getUserBio()}
+            onChange={(e) => this.onChange(e)}
+          ></textarea>
+        </div>
+      </div>
+    );
+  }
+
+  removeTag = (i) => {
+    const newTags = [...this.state.tags];
+    newTags.splice(i, 1);
+    this.setState({ tags: newTags });
+  };
+
+  addTag = () => {
+    const val = this.state.temptag;
+    this.tagInput.value = null;
+    if (val === "") {
+      return;
+    }
+    if (
+      this.state.tags.find(
+        (tag) => tag.toString().toLowerCase() === val.toString().toLowerCase()
+      )
+    ) {
+      return;
+    }
+    this.setState({ tags: [...this.state.tags, this.state.temptag] });
+  };
+
+  tagInputChange = (e) => {
+    this.setState({
+      temptag: [e.target.value],
+    });
+  };
+
+  interestSection() {
+    const { tags } = this.state;
+    return (
+      <div className=" row mb-3">
+        <div className="col-10    ">
+          <InputLabel>Interests</InputLabel>
+          <div className="input-tag">
+            <ul className="input-tag__tags">
+              {tags.map((tag, i) => (
+                <li key={tag}>
+                  {tag}
+                  <IconButton
+                    fontSize="small"
+                    style={{ padding: "5px" }}
+                    type="button"
+                    onClick={() => {
+                      this.removeTag(i);
+                    }}
+                  >
+                    <CloseIcon fontSize="small" />
+                  </IconButton>
+                </li>
+              ))}
+              <li className="input-tag__tags__input">
+                <input
+                  type="text"
+                  onChange={this.tagInputChange}
+                  ref={(c) => {
+                    this.tagInput = c;
+                  }}
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-2 pt-3">
+          <IconButton type="button" onClick={this.addTag} fontSize="large">
+            <AddCircleIcon fontSize="large" />
+          </IconButton>
         </div>
       </div>
     );
