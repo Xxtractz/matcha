d2a6e4c38dee56f2601ffcc2c33d7b285eb421ea
Musa Baloyi <musambaloyi@gmail.com>
2020-05-02 14:28:07 +0200

    adding name Section to Complete Profile

--- client/src/components/Account/AccountComponents/completeProfile.jsx
+++ client/src/components/Account/AccountComponents/completeProfile.jsx
@@ -1,7 +1,83 @@
 import React, { Component } from "react";
-import { Paper } from "@material-ui/core";
+import { Paper, InputLabel } from "@material-ui/core";
+import { Button, TextField, Select } from "@material-ui/core";
 
 class CompleteProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: ["Input"],
+      temptag: "",
+      isopen: true,
+      firstname: getUserFirstName(),
+      lastname: getUserLastName(),
+      gender: getUserGender(),
+      genderPreference: getUserGenderPreference(),
+      interest: getUserInterest(),
+    };
+  }
+
+  imageSection() {
+    return (
+      <div className="container p-2">
+        <div className="col-12 ">
+          <div className="row">
+            <img
+              className="img-thumbnail"
+              src="src/assets/images/addImage.png"
+              alt="imagr"
+              style={{ width: "150px", height: "150px" }}
+            />
+            <input
+              type="button"
+              class="btn btn-secondary"
+              id="btnChangePicture"
+              value="Change"
+            />
+            <input type="file" name="file" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  // Form Sections
+  nameSection() {
+    return (
+      <div className="row mb-3">
+        <div className="col-6">
+          <InputLabel>First Name</InputLabel>
+          <TextField
+            className="col-12"
+            type="text"
+            name="firstname"
+            defaultValue={this.state.firstname}
+            onChange={(e) => this.onChange(e)}
+            required
+          />
+        </div>
+        <div className="col-6 ">
+          <InputLabel>Last Name</InputLabel>
+          <TextField
+            className="col-12"
+            type="text"
+            name="lastname"
+            defaultValue={this.state.lastname}
+            onChange={(e) => this.onChange(e)}
+            required
+          />
+        </div>
+      </div>
+    );
+  }
+
+  personalDetailsSection() {
+    return (
+      <div className="container p-2">
+        <div className="col-12">{this.nameSection()}</div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <Paper
@@ -17,15 +93,9 @@ class CompleteProfile extends Component {
           <small> Please Complete you Profile</small>
         </Paper>
 
-        <div className="container p-2">
-          <div className="col-12 ">
-            <div className="row">
-              <img className="img-thumbnail" src="src/assets/images/addImage.png" alt="imagr"  style={{width: "150px", height: "150px"}} />
-              <input type="button" class="btn btn-secondary" id="btnChangePicture" value="Change" />
-              <input type="file" name="file"/>
-            </div>
-          </div>
-        </div>
+        {this.imageSection()}
+
+        {this.personalDetailsSection()}
       </Paper>
     );
   }
