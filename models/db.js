1dd26f8692cfeff21f2572a48d1dab0c4e9489ed
unknown <pietthabiso@gmail.com>
2020-06-06 17:02:34 +0200

    Users mysql database connecting disconnect

--- models/db.js
+++ models/db.js
@@ -1,19 +1,37 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config");
 
-//creating the connectio to the database here
-const connection = mysql.createConnection({
-    host:dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
-    port: 3306
-});
+let connection;
 
-//opening the mySql connection
-connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-});
+function handleDisconnect() {
+
+    //creating the connectio to the database here
+    connection = mysql.createConnection({
+        host:dbConfig.HOST,
+        user: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        database: dbConfig.DB,
+        port: 3306
+    });
+
+    //opening the mySql connection
+    connection.connect(error => {
+        if (error) {
+            console.log('Error when connecting to database:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+        console.log("Successfully connected to the database.");
+    });
+
+    connection.on('error', (err) => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = connection;
